08f0d59328718375c4213a3c52a596b9e572c331
Krish Kumar <indokrisgraphics@gmail.com>
2026-09-22 23:38:15 +0530

    Time: 0 ms (100%), Space: 11.6 MB (60.33%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        for (int i = digits.size() - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;
+        int n = digits.size();
+        
+        
+        for (int i = n - 1; i >= 0; i--) {
+            // If not 9, just add 1 and we're done
+            if (digits[i] != 9) {
+                digits[i] += 1;
                 return digits;
             }
+            // If it's 9, set to 0 and let the loop carry over
             digits[i] = 0;
         }
-        digits.insert(digits.begin(), 1);
+        
+        // If we get here, all digits were 9 (like [9,9,9])
+        digits[0] = 1;          // change first 0 to 1
+        digits.push_back(0);    // add a 0 at the end -> [1,0,0,0]
         return digits;
     }
 };
